dbe3d263221d919af64bac668fb9681e16fe349f
Ali Sabet <asabet@uwaterloo.ca>
2020-11-05 14:53:47 -0800

    NaN loss produced by model

--- train.sh
+++ train.sh
@@ -1,2 +1,2 @@
-deepspeed rag_train.py ~/gec-data/merged/bea.train.dev.tsv rag --num_workers 4 \
+deepspeed rage_train.py ~/gec-data/merged/bea.train.dev.tsv rag --num_workers 4 \
     --deepspeed --deepspeed_config ds_config.json
